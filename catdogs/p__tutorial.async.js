b7f50b68f60730dae0fe1d0e1713f460b6826d0b
Randall <randall@duomai.com>
2019-10-23 09:57:52 +0800

    [deploy] auto deploy to catdogs at 2019-10-23 09:57:24

--- catdogs/p__tutorial.async.js
+++ catdogs/p__tutorial.async.js
@@ -1,1 +1,1 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[9],{tlQg:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return s});var r=t("q1tI"),a=t.n(r);class s extends a.a.Component{render(){return a.a.createElement("div",null,"This is tutorial page")}}}}]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[8],{tlQg:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return s});var r=t("q1tI"),a=t.n(r);class s extends a.a.Component{render(){return a.a.createElement("div",null,"This is tutorial page")}}}}]);
